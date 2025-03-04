e3faab7c2c0368b05a4440eee8c631fb8246572d
Noah Prince <chewinglass123@gmail.com>
2025-03-04 12:17:50 -0800

    Bugfix return tasks

--- solana-programs/programs/tuktuk/src/instructions/return_tasks_v0.rs
+++ solana-programs/programs/tuktuk/src/instructions/return_tasks_v0.rs
@@ -5,16 +5,18 @@ use super::{RunTaskReturnV0, TaskReturnV0};
 /// Passthrough: Just returns the tasks passed to it as args.
 /// This is useful for remote transactions to schedule themselves.
 #[derive(Accounts)]
-pub struct ReturnTasksV0 {}
+pub struct ReturnTasksV0<'info> {
+    pub system_program: Program<'info, System>,
+}
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Default)]
 pub struct ReturnTasksArgsV0 {
     pub tasks: Vec<TaskReturnV0>,
 }
 
-pub fn handler(_ctx: Context<ReturnTasksV0>, _args: ReturnTasksArgsV0) -> Result<RunTaskReturnV0> {
+pub fn handler(_ctx: Context<ReturnTasksV0>, args: ReturnTasksArgsV0) -> Result<RunTaskReturnV0> {
     Ok(RunTaskReturnV0 {
-        tasks: _args.tasks,
+        tasks: args.tasks,
         tasks_accounts: vec![],
     })
 }
